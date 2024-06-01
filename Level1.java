173c360e688769a6d8ceb3df1a786eead34e9149
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-06-01 23:38:06 +0300

    Add files via upload

---
+++ Level1.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Level1 {
+    Level1() {
+    };
+
+    public static int myfact(int num) {
+        if (num == 1) return 1;
+        int res = 1;
+        for (int i = 2; i <= num; i++) {
+            res = res * (i);
+        }
+        return res;
+    }
+
+    public static int squirrel(int N) {
+        int fact = myfact(N);       // N! nuts
+
+        while (fact >= 10) {        // looking for the first num
+            fact = fact / 10;       // delete last num
+        }
+
+        return fact;
+    }
+
+
+}
+
+
+
+        public static void main (String[]args){
+
+            Level1 lev = new Level1();
+            lev.squirrel(0);
+        }
+
+    }
