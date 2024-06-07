2f259dd6c7c142df5a03c5bb9ef8527ee63b68bb
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-06-07 21:48:43 +0300

    Add files via upload

---
+++ Tarea4/Level1.java
@@ -0,0 +1,71 @@
+public class Level1 {
+
+    public static int [] funcsort (int [] tele1, int L) {
+        //int L = tele1.length;
+        int Lmin = 0;
+        int[] tele2 = new int[L];
+
+        for (int i = 0; i < L; i++) {
+            if (i == 0) {
+                Lmin = tele1[0];
+                tele2[0] =  Lmin;
+            }
+            else {
+                if (tele1[i] < Lmin) {
+                    Lmin = tele1[i];
+
+                    int index = i;
+                    while (index > 0) {
+                        if (tele2 [index-1] > Lmin) {
+
+                            tele2 [index] = tele2 [index-1];
+                            tele2 [index - 1] = Lmin;
+                        }
+                        else
+                        break;//****
+
+                        index--;
+                        }
+                    }
+                else tele2[i] = tele1[i];
+                }
+            Lmin = tele2[i];
+            }
+         return tele2;
+
+    }
+
+    public static int [] funcreverc (int [] tele2, int N) {
+        int  N2 = N/2;
+        int [] newtele = tele2.clone();
+        int k = N-1;
+
+        for (int i = N2; i< N; i++) {
+            int temp = newtele [i];
+            newtele[i] = newtele [k];
+            newtele [k] = temp;
+            k--;
+            if (k - i <= 1) break; // nechet count of arr
+        }
+
+     return newtele;
+    }
+
+
+    public static int [] MadMax (int N, int [] Tele) {
+        int N1 = N/2;
+        int N2 = N - N1;
+
+        int[] newtele = funcsort (Tele, N);
+        int[] newtele1 = funcreverc (newtele, N);
+
+        return newtele1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] ishmatrix  = {10,2,13,19,5,16,7,8,18};
+        MadMax (9, ishmatrix);
+    }
+}
