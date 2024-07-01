90a091b243b04c783a5105bdcd9dda333d290aa3
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-07-02 01:11:14 +0300

    Update Level1.java

--- Tarea15/Level1.java
+++ Tarea15/Level1.java
@@ -61,18 +61,6 @@ public class Level1
         return res;
     }
 
-    public static boolean ifsubarr (int [] array1 , int[] array2, int x , int y ) {
-        int i, j = 0;
-        for (i = 0; i < y; i++) {
-            for (j = 0; j < x; j++)
-                if (array2[i] == array1[j])
-                    break;
-            if (j == x)
-                return false;
-        }
-        return true;
-    }
-
     public static boolean TankRush(int H1, int W1, String S1, int H2, int W2, String S2)
     {
         boolean fret = false;
@@ -96,102 +84,9 @@ public class Level1
     }
 
 
-
-
-    public static void main(String[] args) {
-
-        String S111 = "1234567 2345567 0987567 0999568";
-        String S221 = "10";
-
-        boolean f = TankRush (4, 7, S111, 1, 2, S221);
-        System.out.println(f);
-    }
 }
 
 
-    /*
-
-     public static int [][] StringsToFlagArray (String []S11, String []s22, int H1, int H2, int x) {
-    int i = 0;
-    int j = 0;
-    String subsa;
-    int index = 0;
-    int n = -1;
-    int k = 1;
-    int [][] tab  = new int [x][x];
-
-      for (i = 0; i< x; i++)
-          for (j = 0; j < x; j++) {
-              tab [i][j] = 0;
-          }
 
 
-      for (i = 0; i < H2; i++) {                              // цикл по мал массиву 34  98
-          subsa = new String(s22[i]);                    // переход на след строку
 
-          for (j = i; j < H1; j++) {                           // цикл по бол массиву  1234 2345 0987 - поиск малой строки
-              String sa = new String(S11[j]);
-
-              for (int y = 0; y < sa.length(); y++) {
-                  n = sa.indexOf(subsa, index);  // int ind ex Of ( S tring str, int starti nd ex
-                  if (n >= 0) {
-                      tab[j][n] = k;
-                      index = n + 1;
-                      n = -1;
-                  }
-              }
-              index = 0;
-          }
-          k++;
-      }
-      return tab;
-  }
-
-     public static int [][] transp (int [][] tab, int x) {
-        int i = 0;
-        int j = 0;
-
-        for (i = 0; i < x; i++) {
-            for (j = i + 1; j < x; j++) {
-                int temp = tab[i][j];
-                tab[i][j] = tab[j][i];
-                tab[j][i] = temp;
-            }
-        }
-        return tab;
-    }
-    if (ind1[i] >= 0) {                                      //  найдена
-                    String s = sa.replaceAll(subsa, " ");     // заменить найденную подстроку на "  ";
-                    S11[j] = s;
-
-                    if (naydeno == true) {      //  Уже было
-                        if (ind1[i] == ind1[i - 1]) {
-                            f = true;
-                            break;
-                        }
-                        else {                // уже было но индекс след - другой
-                             i = -1;            // сначала все - ищем подстроку еще раз - сначада
-                            f = false;
-                            naydeno = false;
-                            break;
-                        }
-                    }
-                    else {
-
-                        naydeno = true;
-                        continue;
-                    }
-                }
-                else {
-                        if(naydeno == false) // просто не найдено первый раз
-                            continue;
-                        else {
-                            i = -1;            // сначала все - ищем подстроку еще раз - сначада
-                            f = false;
-                            naydeno = false;
-                            break;
-                        }
-
-
-                    }
-            }*/
