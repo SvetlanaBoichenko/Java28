0fcb09b32cbefbef49f97de10d4e730ed5a63bb4
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-07-19 12:22:08 +0300

    Add files via upload

---
+++ Tarea16/Level1.java
@@ -0,0 +1,59 @@
+public class Level1
+{
+    public static int [] funcsortback (int [] arr1, int L) {
+        int Lmax = 0;
+        int[] arr2 = new int[L];
+
+        for (int i = 0; i < L; i++) {
+            if (i == 0) {
+                Lmax = arr1[0];
+                arr2[0] =  Lmax;
+            }
+            else {
+                if (arr1[i] > Lmax) { //<
+                    Lmax = arr1[i];
+
+                    int index = i;
+                    for (index = i; index > 0; index-- ) {
+                        if (arr2 [index-1] < Lmax) { // >
+
+                            arr2 [index] = arr2 [index-1];
+                            arr2 [index - 1] = Lmax;
+                        }
+                        else
+                            break;//
+
+                        //index--;
+                    }
+                }
+                else arr2[i] = arr1[i];
+            }
+            Lmax = arr2[i];
+        }
+        return arr2;
+    }
+
+
+    public static int MaximumDiscount(int N, int [] price)
+    {
+        int [] B =  funcsortback (price, N);
+        int bsum = 0;
+
+        for (int i = 1; i <= N; i++ ) {
+            if (i % 3 == 0) {
+                bsum += B[i-1];
+            }
+        }
+
+        return bsum ;
+    }
+
+    public static void main(String[] args) {
+        int [] A = {2,5,7,12,23,15,11,4,6, 24};
+
+        int [] B =  funcsortback (A, 10);
+        int s = MaximumDiscount(10, A);
+
+        System.out.println(s);
+    }
+}
