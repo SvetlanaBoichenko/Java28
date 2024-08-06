4120af929c1833b35629cc129efd304e6ba7e083
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2024-08-07 02:21:41 +0300

    Add files via upload

---
+++ Tarea23/Level1Test.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Level1Test {
+
+    @Test
+    public void test1() {
+        String[] wres = {".+..","..+.",".+.."};
+        String[] res = Level1.TreeOfLife(3,4, 12, wres);
+        assertArrayEquals (wres, res);
+    }
+
+    @Test
+    public void test2() {
+        String[] wres = {".+..","..+.",".+.."};
+        String[] res = Level1.TreeOfLife(3,4, 4, wres);
+        assertArrayEquals (wres, res);
+    }
+
+    @Test
+    public void test5() {
+        String[] ish = {"..+..","...+.","..+..","..+.."};
+
+        String[] wres = {"+++++","+++++","+++++","+++++"};
+
+        String[] res = Level1.TreeOfLife(4,5, 5, wres);
+        assertArrayEquals (wres, res);
+    }
+
+    @Test
+    public void test6() {
+        String[] ish = {"+...+","++.++","+...+","+...+"};
+
+        String[] wres = {"..+..",".....","..+..","..+.."};
+
+        String[] res = Level1.TreeOfLife(4,5, 6, ish);
+        assertArrayEquals (wres, res);
+    }
+
+}
